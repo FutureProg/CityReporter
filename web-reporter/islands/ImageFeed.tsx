1d7524f28d30d7cc57df024c700abb593416cde9
Nick Morrison <nickmorrison09@gmail.com>
2026-09-02 19:20:59 -0400

    scrollbar now scrolls right when an image is added

--- web-reporter/islands/ImageFeed.tsx
+++ web-reporter/islands/ImageFeed.tsx
@@ -1,17 +1,28 @@
 import styles from '@/islands/ImageFeed.module.css';
 import { ImagesSignal } from "@/signals.ts";
+import { useRef } from "preact/hooks";
+import { effect } from "@preact/signals";
 
 export type ImageFeedProps = {
     imagesSignal: ImagesSignal;
 }
 
 export const ImageFeed = ({imagesSignal: images}: ImageFeedProps) => {
-    const imagePreview = images.value.map((imgBlob, idx) => {     
+    const viewRef = useRef<HTMLDivElement>(null);
+    const prevImageCount = useRef<number>(images.peek().length);
+    effect(() => {
+        if (prevImageCount.current < images.value.length && viewRef.current) {
+            viewRef.current.scrollLeft = viewRef.current.scrollWidth + 40;
+        }
+        prevImageCount.current = images.value.length;
+    });
+    const imagePreview = images.value.map((imgBlob, idx) => {
         const imageUrl = URL.createObjectURL(imgBlob) 
         return <img src={imageUrl} key={idx} />
-    });
+    });    
+    
     return (
-        <div className={styles.view}>
+        <div className={styles.view} ref={viewRef}>
             {imagePreview}
         </div>
     )
